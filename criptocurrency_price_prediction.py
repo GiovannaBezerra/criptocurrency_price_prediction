3a8139a2093011295f5277c871d2252779be482a
Giovanna Bezerra <giovanna.imeusp@gmail.com>
2023-01-10 11:57:43 -0300

    first commit

---
+++ criptocurrency_price_prediction.py
@@ -0,0 +1,13 @@
+# Criptocurrency Price Prediction with AUTO TS
+
+#https://thecleverprogrammer.com/2021/12/27/cryptocurrency-price-prediction-with-machine-learning/
+
+# To do:
+# 1. Source:https://finance.yahoo.com/
+# 2. API: https://github.com/ranaroussi/yfinance; https://pypi.org/project/yfinance/
+# 3. Create predict model
+# 4. Split data test and train
+# 5. Compare results to verify accuracy...
+# 6. Create a dashboard to show results
+
+
