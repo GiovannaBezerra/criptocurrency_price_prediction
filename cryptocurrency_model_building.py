60b28d82fccfb18923b6abff5b121e8552c01d2b
Giovanna Bezerra <giovanna.imeusp@gmail.com>
2023-03-22 17:24:35 -0300

    reducing the volume of data to run the model

--- cryptocurrency_model_building.py
+++ cryptocurrency_model_building.py
@@ -4,6 +4,8 @@
 # AUTO TS is an open-source python library used to automate Time Series Forecasting. It can automatically train multiple 
 # time series models using a single line of code. 
 # Source: https://www.analyticsvidhya.com/blog/2021/04/automate-time-series-forecasting-using-auto-ts/
+# https://pypi.org/project/AutoTS/
+
 
 # The data was splited into train and test (considering 90:10 ratio). 
 # To simplify the model, was considered historical prices from YFinance since 2021.
@@ -17,11 +19,14 @@ exec(open('cryptocurrency_get_data.py').read())
 
 ### Splitting training and test data:
 
-# Using just the feature named "close" which is the daily closing price:
-df = df_btc[["date", "close"]]
-
 # Sortting values by date:
-df = df.sort_values('date')
+df_btc = df_btc.sort_values('date')
+
+# Select few date to run model:
+df = df_btc.loc[df_btc.date >= '2023-01-01'].reset_index(drop=True)
+
+# Using just the feature named "close" which is the daily closing price:
+df = df[["date", "close"]]
 
 # Using ratio 90:10 to split test and train data. It means 90% of the data for training and 10% for test:
 count_lines = len(df.index)
